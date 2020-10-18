9dcfeafb35dd0c8c13808d6e91ac6f5bba4a6bcb
ailo21 <alex92ailo@gmail.com>
2020-10-18 03:04:30 +0300

    Updates

---
+++ precache-manifest.a293a37969230b8ae6d4794a07e6878c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886481611a2729cdc3412404618c709d",
+    "url": "/level2react/index.html"
+  },
+  {
+    "revision": "ab32c0a0e52d1b0b4c3b",
+    "url": "/level2react/static/css/main.79c0ded9.chunk.css"
+  },
+  {
+    "revision": "3d7ccc3b102bcd16fb32",
+    "url": "/level2react/static/js/2.1d46fd95.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/level2react/static/js/2.1d46fd95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab32c0a0e52d1b0b4c3b",
+    "url": "/level2react/static/js/main.cc1e1be2.chunk.js"
+  },
+  {
+    "revision": "3b5842d7858553f86db2",
+    "url": "/level2react/static/js/runtime-main.21d69399.js"
+  },
+  {
+    "revision": "1d7cb3538ba2ff056172f66fcf8e51d5",
+    "url": "/level2react/static/media/Exo20-Thin.1d7cb353.eot"
+  },
+  {
+    "revision": "44fbebfce5c95819cb765809cfe24b71",
+    "url": "/level2react/static/media/Exo20-Thin.44fbebfc.woff"
+  },
+  {
+    "revision": "b8fd437dca4b38408c905606dd534024",
+    "url": "/level2react/static/media/Exo20-Thin.b8fd437d.ttf"
+  },
+  {
+    "revision": "f782a444c9877f04dce834988e8838cb",
+    "url": "/level2react/static/media/Pattern.f782a444.png"
+  },
+  {
+    "revision": "7afeaf2b8e5fee8b3332318a9f216092",
+    "url": "/level2react/static/media/TrebuchetMS-Bold.7afeaf2b.ttf"
+  },
+  {
+    "revision": "9b45a5e6fd7e43502d74c1cf2bb77ded",
+    "url": "/level2react/static/media/TrebuchetMS-Bold.9b45a5e6.woff"
+  },
+  {
+    "revision": "ca7ac163de240c9e7d6fc018ad0f4dad",
+    "url": "/level2react/static/media/TrebuchetMS-Bold.ca7ac163.eot"
+  },
+  {
+    "revision": "34723a2fbef5882977e8392d020956af",
+    "url": "/level2react/static/media/TrebuchetMS.34723a2f.woff"
+  },
+  {
+    "revision": "530d3166bc7f3ee970eec68df2a2bd2e",
+    "url": "/level2react/static/media/TrebuchetMS.530d3166.ttf"
+  },
+  {
+    "revision": "be77a7db37f37471cff8616dc5b875c5",
+    "url": "/level2react/static/media/TrebuchetMS.be77a7db.eot"
+  },
+  {
+    "revision": "f5d1240b96fa3197aeb9622c3b99d084",
+    "url": "/level2react/static/media/cat.f5d1240b.png"
+  },
+  {
+    "revision": "4008fe1793974242bb4756984dfe82c3",
+    "url": "/level2react/static/media/cat__opacity.4008fe17.png"
+  }
+]);
